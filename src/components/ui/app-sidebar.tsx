3cba7e5509b8581115c7dbef81e8fef8718ece28
rxghavc <r.commandur@gmail.com>
2025-08-08 20:51:09 +0100

    deleted old icon and added new icon, updated emails to use new company email

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
-import { GalleryVerticalEnd, Users, Layers, User, ScrollText } from "lucide-react"
-import { FaUserFriends } from "react-icons/fa";
+import { GalleryVerticalEnd, Users, Layers, User, ScrollText, Mail } from "lucide-react"
+import Image from "next/image";
 
 import {
   Sidebar,
@@ -52,8 +52,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <a href="/">
-                <div className="text-emerald-500 flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <FaUserFriends className="size-8" />
+                <div className="flex items-center justify-center rounded-lg overflow-hidden">
+                  <Image
+                    src="/grouppot_logo.png"
+                    alt="GroupPot logo"
+                    width={32}
+                    height={32}
+                    priority
+                  />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-medium">GroupPot</span>
@@ -86,21 +92,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             This app was built as a polling system solution for my father and his friends, I hope you enjoy it!
           </div>
           <a
-            href="https://linkedin.com/in/raghavcommandur"
+            href="mailto:info.grouppot@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="View LinkedIn"
+            aria-label="Contact Us"
             className="flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-emerald-500"
           >
-            <svg
-              width="20"
-              height="20"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.025-3.063-1.868-3.063-1.868 0-2.154 1.459-2.154 2.968v5.699h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.838-1.563 3.036 0 3.6 2 3.6 4.59v5.606z" />
-            </svg>
-            <span>View LinkedIn</span>
+            <Mail className="w-5 h-5" />
+            <span>Contact Us</span>
           </a>
         </div>
       </SidebarFooter>
